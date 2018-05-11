f66c8c10be12c0894d1ca0f55910213eb7491c27
Robert Hall <rob@csart.space>
2018-05-11 16:16:25 +0100

    Test reused text scoll stuff

--- sketch.js
+++ sketch.js
@@ -1,7 +1,56 @@
+
+// create vars for elements
+
+var title, countryTitle, countryDesc, countryImg, countryVid, countryLastWin, countryOrder, countryFlag;
+var textScrollers = [10];
+
+
+
+
 function setup(){
+
 
 }
 
 function draw(){
+  scrollTextUpdate();
+}
 
+function onMouseDown(){
+  scrollTextInit("WORDS");
+}
+
+function scrollTextInit(message){
+    for(var i = 0; i<textScrollers.length; i++){
+        if(textScrollers[i] == null){
+            console.log("1");
+            textScrollers[i] = {message: message, posx: 0-(message.length*60), posy: random(innerHeight-100)};
+            textScrollers[i].element = createElement('h1',message);
+            textScrollers[i].element.position(textScrollers[i].posx,textScrollers[i].posy);
+            textScrollers[i].element.style('font-family','sans-serif');
+            textScrollers[i].element.style('font-size','72pt');
+            return true;
+        }
+    }
+    console.log("2");
+    var newScroller = textScrollers.length;
+    textScrollers[newScroller] = {message: message, posx: 0-(message.length*60), posy: random(innerHeight-100)};
+    textScrollers[newScroller].element = createElement('h1',message);
+    textScrollers[newScroller].element.position(textScrollers[newScroller].posx,textScrollers[newScroller].posy);
+    textScrollers[newScroller].element.style('font-family','sans-serif');
+    textScrollers[newScroller].element.style('font-size','72pt');
+}
+
+function scrollTextUpdate(){
+    for(var i = 0; i<textScrollers.length; i++){
+        if(textScrollers[i] != null){
+            if(textScrollers[i].posx>innerWidth){
+                textScrollers[i].element.remove();
+                textScrollers[i] = null;
+            } else {
+                textScrollers[i].element.position(textScrollers[i].posx,textScrollers[i].posy);
+                textScrollers[i].posx+=2;
+            }
+        }
+    }
 }
