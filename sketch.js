64f03606f5324fdd435f5bd781e3c6a9da1f5c9f
Robert Hall <rob@csart.space>
2018-05-12 18:58:01 +0100

    final

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ var countries = [{
   {
     name: "Spain",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "http://ichef.bbci.co.uk/wwfeatures/wm/live/1280_640/images/live/p0/51/y4/p051y4n8.jpg",
     lastWin: 1969,
     flag: "🇪🇸",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -24,7 +24,7 @@ var countries = [{
   {
     name: "Slovenia",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.slovenia.info/uploads/turisticna/Alpine-Slovenia-bled.jpg",
     lastWin: "Never",
     flag: "🇸🇮",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -32,7 +32,7 @@ var countries = [{
   {
     name: "Lithuania",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.avrupa.info.tr/sites/default/files/styles/standard_page_header/public/2016-08/Lithuania%20Vilnius.jpg?itok=eCQaoGd_",
     lastWin: "Never",
     flag: "🇱🇹",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -40,7 +40,7 @@ var countries = [{
   {
     name: "Austria",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.nationalgeographic.com/content/dam/travel/Guide-Pages/europe/austria/Austria_NationalGeographic_2618017.jpg",
     lastWin: 2014,
     flag: "🇦🇹",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -48,7 +48,7 @@ var countries = [{
   {
     name: "Estonia",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://static2.visitestonia.com/images/3129372/1000_500_false_false_83b107718934b834a1c9edf3b09a4123.jpg",
     lastWin: 2001,
     flag: "🇪🇪",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -56,7 +56,7 @@ var countries = [{
   {
     name: "Norway",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.worldatlas.com/r/w728-h425-c728x425/upload/04/92/d1/shutterstock-382716112.jpg",
     lastWin: 2009,
     flag: "🇳🇴",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -64,7 +64,7 @@ var countries = [{
   {
     name: "Portugal",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://mansion-global-app.s3.amazonaws.com/stories/portugal-special-report-round-up/media/portugalroundup_lead-mr.jpg",
     lastWin: 2017,
     flag: "🇵🇹",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -72,7 +72,7 @@ var countries = [{
   {
     name: "United Kingdom",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "http://www.nationsonline.org/gallery/UK/Palace-of-Westminster-Parliament.jpg",
     lastWin: 1997,
     flag: "🇬🇧",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -80,7 +80,7 @@ var countries = [{
   {
     name: "Serbia",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "http://www.nationsonline.org/gallery/Serbia/Novi-Sad-City-Hall.jpg",
     lastWin: 2007,
     flag: "🇷🇸",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -88,7 +88,7 @@ var countries = [{
   {
     name: "Germany",
     desc: "Interestingly also won in 82 as west germany!",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2018/03/21/105079854-GettyImages-726792551.1910x1000.jpg?v=1521651738",
     lastWin: 2010,
     flag: "🇩🇪",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -96,7 +96,7 @@ var countries = [{
   {
     name: "Albania",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "http://www.moveabroadnow.com/wp-content/uploads/albania1.jpg",
     lastWin: "Never",
     flag: "🇦🇱",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -104,7 +104,7 @@ var countries = [{
   {
     name: "France",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://cdn.travel-mediaserver.com/driveaway/images/car-hire/destinations/france-large.jpg",
     lastWin: 1977,
     flag: "🇫🇷",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -112,7 +112,7 @@ var countries = [{
   {
     name: "Czech Republic",
     desc: "Best : 25th, in 2016",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.trafalgar.com/~/media/images/home/destinations/europe-and-britain/czech-republic/2016-licensed-images/czechrepublic-prague-cityscape-2016-hero-l-482676419.jpg?mw=1200&",
     lastWin: "Never",
     flag: "🇨🇿",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -120,7 +120,7 @@ var countries = [{
   {
     name: "Denmark",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.worldtravelguide.net/wp-content/uploads/2017/04/Think-Denmark-Copenhagen-587892190-SeanPavonePhoto-copy.jpg",
     lastWin: 2013,
     flag: "🇩🇰",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -128,7 +128,7 @@ var countries = [{
   {
     name: "Australia",
     desc: "Came 2nd in 2016, only been here since 2015",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Australia_Day.jpg/1200px-Australia_Day.jpg",
     lastWin: "Never",
     flag: "🇦🇺",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -136,7 +136,7 @@ var countries = [{
   {
     name: "Finland",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.usnews.com/dims4/USNEWS/96415fc/2147483647/crop/4050x2700%2B0%2B0/resize/1200x1200%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F7d%2F1e%2F3b7a4be94b799c734b853d3f764c%2Fbc17-finland-gallery-14.JPG",
     lastWin: 2006,
     flag: "🇫🇮",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -144,7 +144,7 @@ var countries = [{
   {
     name: "Bulgaria",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://globalgrasshopper.com/wp-content/uploads/2013/04/Sozopol.png",
     lastWin: "Never",
     flag: "🇧🇬",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -152,7 +152,7 @@ var countries = [{
   {
     name: "Moldova",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "http://www.publicfinanceinternational.org/sites/default/files/web_chisinau_shutterstock_459050377.jpg",
     lastWin: "Never",
     flag: "🇲🇩",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -160,7 +160,7 @@ var countries = [{
   {
     name: "Sweden",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://assets3.thrillist.com/v1/image/2706821/size/gn-gift_guide_variable_c.jpg",
     lastWin: 2015,
     flag: "🇸🇪",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -168,7 +168,7 @@ var countries = [{
   {
     name: "Hungary",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.worldatlas.com/r/w728-h425-c728x425/upload/b1/ff/c9/thinkstockphotos-473481530.jpg",
     lastWin: "Never",
     flag: "🇭🇺",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -176,7 +176,7 @@ var countries = [{
   {
     name: "Israel",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://timedotcom.files.wordpress.com/2016/12/gettyimages-511828219.jpg",
     lastWin: 1998,
     flag: "🇮🇱",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -184,7 +184,7 @@ var countries = [{
   {
     name: "The Netherlands",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.iamexpat.nl/sites/default/files/styles/article--full/public/netherlands-rentals.jpg?itok=BsbdeA6Q",
     lastWin: 1975,
     flag: "🇳🇱",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -192,7 +192,7 @@ var countries = [{
   {
     name: "Ireland",
     desc: "They won a bunch in the 90s but have come last in the noughties",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://cdn.tourradar.com/s3/tour/original/101761_91f4da0e.jpg",
     lastWin: 1996,
     flag: "🇮🇪",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -200,7 +200,7 @@ var countries = [{
   {
     name: "Cyprus",
     desc: "Just english people",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://cdn.purpleinternational.eu/blog_images/57/cyprus.jpg",
     lastWin: "Never",
     flag: "🇨🇾",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -208,7 +208,7 @@ var countries = [{
   {
     name: "Italy",
     desc: "Last year was a banger!",
-    img: "https://emerging-europe.com/wp-content/uploads/2016/11/bigstock-140187299-1024x682.jpg",
+    img: "https://www.italymototour.com/wp-content/uploads/2017/05/positano.jpg",
     lastWin: 1990,
     flag: "🇮🇹",
     vid: "https://www.youtube.com/embed/DJ9ajcic9fI"
@@ -260,7 +260,7 @@ function draw() {
 }
 
 function touchStarted() {
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < 30; i++) {
     flagEmoji.push(new flag(mouseX, mouseY,3));
   }
 }
@@ -375,7 +375,7 @@ function setHTML(_scene) {
 
 function bigButtonHandler() {
   setHTML("countryData");
-  for (var i = 0; i < 300; i++) {
+  for (var i = 0; i < 100; i++) {
     flagEmoji.push(new flag(innerWidth / 2, 200,20,countryFromCookie));
   }
 }
@@ -384,7 +384,7 @@ function resetButtonHandler() {
   countryFromCookie = Math.floor(Math.random() * 25);
   document.cookie = countryFromCookie;
   setHTML("countryData");
-  for (var i = 0; i < 300; i++) {
+  for (var i = 0; i < 100; i++) {
     flagEmoji.push(new flag(innerWidth / 2, 200,20,countryFromCookie));
   }
 }
