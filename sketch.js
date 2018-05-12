2b3f601d101911104c4752e174a96d1144250106
Robert Hall <rob@csart.space>
2018-05-12 13:01:08 +0100

    why u no commit

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // create vars for elements
 
-var title, countryTitle, countryDesc, countryImg, countryVid, countryLastWin, countryOrder, countryFlag;
+var title, countryTitle, countryDesc, countryImg, countryVid, countryLastWin, countryOrder, countryFlag, bigButton;
 var flagEmoji = [];
 
 var countryFromCookie;
@@ -193,50 +193,50 @@ var countries = [{
 
 function setup() {
 
-  if (document.cookie != null){
+  if (document.cookie != null) {
     countryFromCookie = document.cookie;
   } else {
-    countryFromCookie = random(0,countries.length);
+    countryFromCookie = random(0, countries.length);
     document.cookie = countryFromCookie;
   }
 
-  for(var i = 0; i<100; i++){
-    flagEmoji.push(new flag(innerWidth/2,innerHeight/2));
+  for (var i = 0; i < 100; i++) {
+    flagEmoji.push(new flag(innerWidth / 2, innerHeight / 2));
   }
 
 }
 
 function draw() {
 
-  for(var i = 0; i < flagEmoji.length; i++){
+  for (var i = 0; i < flagEmoji.length; i++) {
     flagEmoji[i].update();
   }
 
-  for(var i = 0; i < flagEmoji.length; i++){
-    if(flagEmoji[i].age<0){
+  for (var i = 0; i < flagEmoji.length; i++) {
+    if (flagEmoji[i].age < 0) {
       flagEmoji[i].kill();
-      flagEmoji.splice(i,1);
+      flagEmoji.splice(i, 1);
     }
   }
 }
 
-function mousePressed(){
-  for(var i = 0; i<300; i++){
-    flagEmoji.push(new flag(mouseX,mouseY));
+function mousePressed() {
+  for (var i = 0; i < 50; i++) {
+    flagEmoji.push(new flag(mouseX, mouseY));
   }
 }
 
-function flag(_x,_y) {
+function flag(_x, _y) {
 
   this.position = createVector(_x, _y);
-  this.country = random(0, 25);
-  this.speed = createVector(random(-5, 5),random(-5, 5)).limit(5);
+  this.country = Math.floor(Math.random() * countries.length);
+  this.speed = createVector(random(-5, 5), random(-5, 5)).limit(3);
   this.size = 10;
-  this.age = random(3,7);
-  this.flagElement = createP(countries[Math.floor(Math.random()*countries.length)].flag);
+  this.age = random(1, 3);
+  this.flagElement = createP(countries[this.country].flag);
 
   this.update = function() {
-    this.age-=random(0.01,0.1);
+    this.age -= random(0.01, 0.1);
     this.physics();
     //text(this.position.x,this.position.y,this.size,this.size,countries[this.country].flag);
     this.flagElement.position(this.position.x, this.position.y);
@@ -247,7 +247,33 @@ function flag(_x,_y) {
   }
 
   this.physics = function() {
-    this.position.y+=this.speed.y;
-    this.position.x+=this.speed.x;
+    this.position.y += this.speed.y;
+    this.position.x += this.speed.x;
+  }
+}
+
+function setHTML(_scene) {
+  //remove all previous relevant html
+  countryTitle.remove();
+  countryImg.remove();
+  countryVid.remove();
+  countryDesc.remove();
+  countryLastWin.remove();
+  countryOrder.remove();
+  bigButton.remove();
+
+  if (_scene = "choooseButton") {
+    bigButton.createButton("Choose a Country!");
+    bigButton.touchStarted(buttonHandler("bigButton"));
+  }
+  if (_scene = "countryData") {
+
+  }
+
+}
+
+function buttonHandler(_button) {
+  if (_button = "bigButton") {
+    //trigger other stuff like a waterfall of emoji and HTML switch.
   }
 }
